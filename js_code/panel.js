d9d58af711d7c444ce9ed8b37d4ae5391184778c
SpeedCube <chipai_dom@mail.ru>
2022-02-18 12:47:01 +0300

    reorganizacion

--- js_code/panel.js
+++ js_code/panel.js
@@ -1,51 +1,49 @@
-const spravka = document.querySelector('.spravka');
-const rMenu = document.querySelector('.rightMenu');
-const men = document.querySelector('.menuMy');
-const divMenu = document.querySelector('.myMenu');
-const zag = document.getElementById('scrH');
-const par = document.getElementById('scrP');
-
-let coord;
-let koord;
-
-function koordinat() {
-  coord = men.getBoundingClientRect();
-  koord = coord.left;
-  divMenu.style.left = koord + 'px';
-}
-
-function klasN() {
-  zag.className = "hi_on";
-  par.className = "hi_on";
-}
-window.addEventListener('resize', function () {
-  koordinat();
-})
 
 
-//горизонтальная панель
-spravka.addEventListener('click', () => {
-  rMenu.style.width = 218 + 'px';
-  rMenu.style.right = 1 + 'px';
-  setTimeout(klasN, 450);
-})
+// const zag = document.getElementById('scrH');
+// const par = document.getElementById('scrP');
 
-rMenu.addEventListener('click', () => {
-  rMenu.style.width = 0 + 'px';
-  rMenu.style.right = -21 + 'px';
-  zag.className = "hi_off";
-  par.className = "hi_off";
-})
-//вертикальная панель
-men.addEventListener('click', () => {
-  koordinat();
-  divMenu.style.top = 50 + 'px';
-})
+// let coord;
+// let koord;
 
-divMenu.addEventListener('click', () => {
-  koordinat();
-  divMenu.style.top = -300 + 'px';
-})
+// function koordinat() {
+//   coord = men.getBoundingClientRect();
+//   koord = coord.left;
+//   divMenu.style.left = koord + 'px';
+// }
+
+// function klasN() {
+//   zag.className = "hi_on";
+//   par.className = "hi_on";
+// }
+// window.addEventListener('resize', function () {
+//   koordinat();
+// })
+
+
+// //горизонтальная панель
+// spravka.addEventListener('click', () => {
+//   rMenu.style.width = 218 + 'px';
+//   rMenu.style.right = 1 + 'px';
+//   setTimeout(klasN, 450);
+// })
+
+// rMenu.addEventListener('click', () => {
+//   rMenu.style.width = 0 + 'px';
+//   rMenu.style.right = -21 + 'px';
+//   zag.className = "hi_off";
+//   par.className = "hi_off";
+// })
+// //вертикальная панель
+// men.addEventListener('click', () => {
+//   koordinat();
+//   divMenu.style.top = 50 + 'px';
+// })
+
+// divMenu.addEventListener('click', () => {
+//   koordinat();
+//   divMenu.style.top = -300 + 'px';
+// })
 
 //menu burger
 const iconMenu = document.querySelector('.menu__icon');
